04f76b27ad536843dd1869798748cd7427d71463
TH_9s <th.suyanan@gmail.com>
2024-10-11 15:47:01 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+
+}
+</script>
+
+
+<template>
+  <div class="backdrop">
+    <div class="loading-bg">
+      <div class="sun-moon">
+        <div class="sun"></div>
+        <div class="moon"></div>
+      </div>
+      <div class="shift">
+      </div>
+    </div>
+  </div>
+</template>
